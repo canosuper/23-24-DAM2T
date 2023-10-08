6aa35880c9d069acc8ec3164c0ce05e54d2e8f87
ANTONIO C <acanosuper@gmail.com>
2023-10-08 18:51:29 +0200

    Commit Paso datos Varios Activitys

--- VariosActivitys/app/src/main/java/com/example/variosactivitys/MainActivity.kt
+++ VariosActivitys/app/src/main/java/com/example/variosactivitys/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.variosactivitys
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.variosactivitys.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.boton.setOnClickListener {
+            irAVentana2()
+        }
+    }
+
+    private fun irAVentana2() {
+        var miIntent: Intent = Intent(this, Ventana2::class.java)
+        miIntent.putExtra("nombre", binding.cajaNombre.text.toString())
+        miIntent.putExtra("edad", binding.cajaEdad.text.toString())
+        startActivity(miIntent)
     }
 }
--- VariosActivitys/app/src/main/java/com/example/variosactivitys/Ventana2.kt
+++ VariosActivitys/app/src/main/java/com/example/variosactivitys/Ventana2.kt
@@ -2,10 +2,26 @@ package com.example.variosactivitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.variosactivitys.databinding.ActivityVentana2Binding
 
 class Ventana2 : AppCompatActivity() {
+    lateinit var binding: ActivityVentana2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ventana2)
+
+        binding = ActivityVentana2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_ventana2)
+
+        var nombre = intent.getStringExtra("nombre")
+        var edad = intent.getStringExtra("edad")
+        binding.cajaNombre.setText(nombre)
+        binding.cajaEdad.setText(edad)
+
+        //cambiarlo a una Clase Persona serializable, Dataclass en este caso, en un paquete modelo.
+
+        binding.boton.setOnClickListener {
+            finish()
+        }
     }
 }
