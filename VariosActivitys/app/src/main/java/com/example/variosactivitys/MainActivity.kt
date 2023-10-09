73257ccab5ed9a59283943825d4ca86aa8a5f9ad
ANTONIO C <acanosuper@gmail.com>
2023-10-09 14:10:57 +0200

    Añadidos mensajes de Log para comprobar el paso por los estados de las Activitys

--- VariosActivitys/app/src/main/java/com/example/variosactivitys/MainActivity.kt
+++ VariosActivitys/app/src/main/java/com/example/variosactivitys/MainActivity.kt
@@ -3,19 +3,23 @@ package com.example.variosactivitys
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.variosactivitys.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.e("ACSCO", "ONCREATE(), Ventana 1")
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         binding.boton.setOnClickListener {
             irAVentana2()
         }
+
     }
 
     private fun irAVentana2() {
@@ -25,5 +29,27 @@ class MainActivity : AppCompatActivity() {
         miIntent.putExtra("nombre", binding.cajaNombre.text.toString())
         miIntent.putExtra("edad", binding.cajaEdad.text.toString())
         startActivity(miIntent)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("ACSCO", "ONSTART(), Ventana 1")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("ACSCO", "ONRESUME(), Ventana 1")
+    }
+    override fun onPause(){
+        super.onPause()
+        Log.e("ACSCO", "ONPAUSE(), Ventana 1")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.e("ACSCO", "ONSTOP(), Ventana 1")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("ACSCO", "ONDESTROY(), Ventana 1")
     }
 }
--- VariosActivitys/app/src/main/java/com/example/variosactivitys/Ventana2.kt
+++ VariosActivitys/app/src/main/java/com/example/variosactivitys/Ventana2.kt
@@ -2,6 +2,7 @@ package com.example.variosactivitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.variosactivitys.databinding.ActivityVentana2Binding
 import modelo.Persona
 import modelo.Personas
@@ -10,7 +11,7 @@ class Ventana2 : AppCompatActivity() {
     lateinit var binding: ActivityVentana2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        Log.e("ACSCO", "ONCREATE(), Ventana 2")
         binding = ActivityVentana2Binding.inflate(layoutInflater)
         setContentView(binding.root)
         //setContentView(R.layout.activity_ventana2)
@@ -33,5 +34,26 @@ class Ventana2 : AppCompatActivity() {
         binding.boton.setOnClickListener {
             finish()
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.e("ACSCO", "ONSTART(), Ventana 2")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("ACSCO", "ONRESUME(), Ventana 2")
+    }
+    override fun onPause(){
+        super.onPause()
+        Log.e("ACSCO", "ONPAUSE(), Ventana 2")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.e("ACSCO", "ONSTOP(), Ventana 2")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("ACSCO", "ONDESTROY(), Ventana 2")
     }
 }
