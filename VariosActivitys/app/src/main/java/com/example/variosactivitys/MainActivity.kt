216cdd9ba170ff4ec1b0c78c6a5775aa3e9f9f21
ANTONIO C <acanosuper@gmail.com>
2023-10-09 19:53:37 +0200

    Añadidos mensajes de Log para comprobar el paso por los estados de las Activitys

--- VariosActivitys/app/src/main/java/com/example/variosactivitys/MainActivity.kt
+++ VariosActivitys/app/src/main/java/com/example/variosactivitys/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.e("ACSCO", "ONCREATE(), Ventana 1")
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
