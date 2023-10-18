604c110e6bd09ab4e57c9b2b828e878c45e37018
ANTONIO C <acanosuper@gmail.com>
2023-10-18 12:08:11 +0200

    Version Haloween y Control de puntos

--- TresEnRaya/app/src/main/java/com/example/tresenraya/MainActivity.kt
+++ TresEnRaya/app/src/main/java/com/example/tresenraya/MainActivity.kt
@@ -14,10 +14,15 @@ class MainActivity : AppCompatActivity() {
     val matriz = Array(3) { Array(3) { Estado.VACIO } }
     lateinit var binding : ActivityMainBinding
     var turnoJug1: Boolean = true
+    var puntosJug1: Int=0
+    var puntosJug2: Int=0
+    var tirada:Int=0
+    var finPartida:Boolean=false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
        // setContentView(R.layout.activity_main)
 
@@ -56,39 +61,63 @@ class MainActivity : AppCompatActivity() {
 
     private fun reiniciarJuego() {
         //reinicio la matriz
-        for(i in 0 until 3) {
+        for (i in 0 until 3) {
             for (j in 0 until 3) {
                 matriz[i][j] = Estado.VACIO
             }
         }
-        recreate() //reinicio el activity, llama de nuevo a OnCreate
+        turnoJug1 = true
+        binding.imTurno.setImageResource(R.drawable.noviacadaver)
+        tirada=0
+        finPartida=false
+
+        binding.im00.setImageResource(R.drawable.cuadrito)
+        binding.im01.setImageResource(R.drawable.cuadrito)
+        binding.im02.setImageResource(R.drawable.cuadrito)
+        binding.im10.setImageResource(R.drawable.cuadrito)
+        binding.im11.setImageResource(R.drawable.cuadrito)
+        binding.im12.setImageResource(R.drawable.cuadrito)
+        binding.im20.setImageResource(R.drawable.cuadrito)
+        binding.im21.setImageResource(R.drawable.cuadrito)
+        binding.im22.setImageResource(R.drawable.cuadrito)
     }
 
-
     private fun evaluarJuego(i: Int, j: Int) {
-        pintaFicha(i,j)
-        if(turnoJug1) {
-           // binding.im00.setImageResource(R.drawable.ic_sun)
-            matriz[i][j]=Estado.JUG1
-            turnoJug1 = false
-            binding.imTurno.setImageResource(R.drawable.ic_moon)
-        }
-        else{
-           // binding.im00.setImageResource(R.drawable.ic_moon)
-            matriz[i][j]=Estado.JUG2
-            turnoJug1=true
-            binding.imTurno.setImageResource(R.drawable.ic_sun)
-        }
-        if(hayGanador()!=Estado.VACIO){
-            if(hayGanador()==Estado.JUG1){
-                Toast.makeText(this, "GANADOR JUGADOR 1",Toast.LENGTH_SHORT).show()
+        if(!finPartida) {
+            pintaFicha(i, j)
+            tirada++
+            if (turnoJug1) {
+                // binding.im00.setImageResource(R.drawable.ic_sun)
+                matriz[i][j] = Estado.JUG1
+                turnoJug1 = false
+                binding.imTurno.setImageResource(R.drawable.zombi)
+            } else {
+                // binding.im00.setImageResource(R.drawable.ic_moon)
+                matriz[i][j] = Estado.JUG2
+                turnoJug1 = true
+                binding.imTurno.setImageResource(R.drawable.noviacadaver)
             }
-            else{
-                Toast.makeText(this, "GANADOR JUGADOR 2",Toast.LENGTH_SHORT).show()
+            if ((hayGanador() != Estado.VACIO) || (tirada == 9)) {
+                if (hayGanador() == Estado.JUG1) {
+                    finPartida = true
+                    puntosJug1 += 1
+                    binding.txtPuntos1.text = puntosJug1.toString()
+                    Toast.makeText(this, "GANADOR JUGADOR 1", Toast.LENGTH_SHORT).show()
+                } else {
+                    if (hayGanador() == Estado.JUG2) {
+                        finPartida = true
+                        puntosJug2 += 1
+                        binding.txtPuntos2.text = puntosJug2.toString()
+                        Toast.makeText(this, "GANADOR JUGADOR 2", Toast.LENGTH_SHORT).show()
+                    } else {
+                        //no hay ganador y se llega a la tirada 9
+                        finPartida = true
+                        Toast.makeText(this, "¡¡EMPATE!!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                binding.button.visibility = View.VISIBLE
             }
-            binding.button.visibility=View.VISIBLE
         }
-
     }
 
     private fun hayGanador(): Estado {
@@ -126,8 +155,12 @@ class MainActivity : AppCompatActivity() {
             j++
         }
         var e: Estado = Estado.VACIO
-        if(ganaJug1) e=Estado.JUG1
-        if(ganaJug2) e=Estado.JUG2
+        if(ganaJug1) {
+            e=Estado.JUG1
+        }
+        if(ganaJug2){
+            e=Estado.JUG2
+        }
 
         return e
     }
@@ -135,32 +168,51 @@ class MainActivity : AppCompatActivity() {
     private fun pintaFicha(i: Int, j: Int) {
         when {
             i == 0 && j == 0 -> {
-                if(turnoJug1) binding.im00.setImageResource(R.drawable.ic_sun) else binding.im00.setImageResource(R.drawable.ic_moon)
+                if(turnoJug1) binding.im00.setImageResource(R.drawable.noviacadaver) else binding.im00.setImageResource(
+                    R.drawable.zombi
+                )
             }
             i == 0 && j == 1 -> {
-                if(turnoJug1) binding.im01.setImageResource(R.drawable.ic_sun) else binding.im01.setImageResource(R.drawable.ic_moon)
+                if(turnoJug1) binding.im01.setImageResource(R.drawable.noviacadaver) else binding.im01.setImageResource(
+                    R.drawable.zombi
+                )
             }
             i == 0 && j == 2 -> {
-                if(turnoJug1) binding.im02.setImageResource(R.drawable.ic_sun) else binding.im02.setImageResource(R.drawable.ic_moon)
+                if(turnoJug1) binding.im02.setImageResource(R.drawable.noviacadaver) else binding.im02.setImageResource(
+                    R.drawable.zombi
+                )
             }
             i == 1 && j == 0 -> {
-                if(turnoJug1) binding.im10.setImageResource(R.drawable.ic_sun) else binding.im10.setImageResource(R.drawable.ic_moon)
+                if(turnoJug1) binding.im10.setImageResource(R.drawable.noviacadaver) else binding.im10.setImageResource(
+                    R.drawable.zombi
+                )
             }
             i == 1 && j == 1 -> {
-                if(turnoJug1) binding.im11.setImageResource(R.drawable.ic_sun) else binding.im11.setImageResource(R.drawable.ic_moon)
+                if(turnoJug1) binding.im11.setImageResource(R.drawable.noviacadaver) else binding.im11.setImageResource(
+                    R.drawable.zombi
+                )
             }
             i == 1 && j == 2 -> {
-                if(turnoJug1) binding.im12.setImageResource(R.drawable.ic_sun) else binding.im12.setImageResource(R.drawable.ic_moon)
+                if(turnoJug1) binding.im12.setImageResource(R.drawable.noviacadaver) else binding.im12.setImageResource(
+                    R.drawable.zombi
+                )
             }
             i == 2 && j == 0 -> {
-                if(turnoJug1) binding.im20.setImageResource(R.drawable.ic_sun) else binding.im20.setImageResource(R.drawable.ic_moon)
+                if(turnoJug1) binding.im20.setImageResource(R.drawable.noviacadaver) else binding.im20.setImageResource(
+                    R.drawable.zombi
+                )
             }
             i == 2 && j == 1 -> {
-                if(turnoJug1) binding.im21.setImageResource(R.drawable.ic_sun) else binding.im21.setImageResource(R.drawable.ic_moon)
+                if(turnoJug1) binding.im21.setImageResource(R.drawable.noviacadaver) else binding.im21.setImageResource(
+                    R.drawable.zombi
+                )
             }
             i == 2 && j == 2 -> {
-                if(turnoJug1) binding.im22.setImageResource(R.drawable.ic_sun) else binding.im22.setImageResource(R.drawable.ic_moon)
+                if(turnoJug1) binding.im22.setImageResource(R.drawable.noviacadaver) else binding.im22.setImageResource(
+                    R.drawable.zombi
+                )
             }
+
 
 
         }
